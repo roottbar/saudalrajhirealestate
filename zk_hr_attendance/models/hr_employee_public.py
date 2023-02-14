c3d95510fa25029896c94dcf385080f8a2f1a82f
Bashier Elbashier <elbashier97@gmail.com>
2023-02-14 10:59:09 +0300

    [MOD] modify all addons

--- zk_hr_attendance/models/hr_employee_public.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models, api, _
-
-
-class HrEmployeePublic(models.Model):
-    _inherit = "hr.employee.public"
-
-    biometric_device_id = fields.Integer("Attendance Device ID")
