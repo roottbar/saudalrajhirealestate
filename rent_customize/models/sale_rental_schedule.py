539ccd44e6473ef81d481376bd518fd71d87b245
Bashier Elbashier <elbashier97@gmail.com>
2023-03-12 11:15:06 +0300

    [MOD] modify rent_customize

--- rent_customize/models/sale_rental_schedule.py
+++ rent_customize/models/sale_rental_schedule.py
@@ -5,9 +5,9 @@ class SaleRentalSchedule(models.Model):
     _inherit = 'sale.rental.schedule'
 
     property_number = fields.Many2one(related="order_line_id.property_number")
-    property_id = fields.Many2one(related="product_id.property_id")
-    property_address_area = fields.Many2one(related="product_id.property_id.property_address_area")
-    property_address_build = fields.Many2one(related="product_id.property_id.property_address_build")
+    property_id = fields.Many2one(related="product_id.property_id", store=1)
+    property_address_area = fields.Many2one(related="product_id.property_id.property_address_area", store=1)
+    property_address_build = fields.Many2one(related="product_id.property_id.property_address_build", store=1)
     # rental_name = fields.Char(string="Name", compute="get_rental_name")
 
     # @api.depends('order_line_id')
