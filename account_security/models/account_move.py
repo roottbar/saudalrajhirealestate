a242cdb897c9febc37f2a60252f99f2a2517428b
ahmedoosama <ahmed.os.moh@gmail.com>
2023-09-12 20:49:24 +0300

    [MOD]

--- account_security/models/account_move.py
+++ account_security/models/account_move.py
@@ -1,4 +1,5 @@
-from odoo import fields, models, api
+from odoo import fields, models, api,_
+from odoo.exceptions import UserError 
 
 
 class AccountMove(models.Model):
@@ -10,3 +11,28 @@ class AccountMove(models.Model):
     def action_review(self):
         self.sudo().write({'state': 'review'})
 
+    @api.model
+    def create(self, vals):
+        ret = super(AccountMove, self).create(vals)
+        if not self.user_has_groups('account_security.group_account_customer_invoices_create') and ret.move_type =='out_invoice':
+            raise UserError(_("You are not allowed to create"))
+        if not self.user_has_groups('account_security.group_account_vendor_bill_create') and ret.move_type =='in_invoice':
+            raise UserError(_("You are not allowed to create"))
+        if not self.user_has_groups('account_security.group_account_journal_entry_create') and ret.move_type =='entry':
+            raise UserError(_("You are not allowed to create"))
+        return ret
+
+
+    
+    def action_post(self):
+        ret = super(AccountMove, self).action_post()
+        if not self.user_has_groups('account_security.group_account_customer_invoices_confirm') and self.move_type =='out_invoice':
+            raise UserError(_("You are not allowed to confirm"))
+        if not self.user_has_groups('account_security.group_account_vendor_bill_confirm') and self.move_type =='in_invoice':
+            raise UserError(_("You are not allowed to confirm"))
+        if not self.user_has_groups('account_security.group_account_journal_entry_confirm') and self.move_type =='entry':
+            raise UserError(_("You are not allowed to confirm"))
+        return ret
+
+
+    
--- account_security/models/account_payment.py
+++ account_security/models/account_payment.py
@@ -1,4 +1,6 @@
-from odoo import fields, models, api
+from odoo import fields, models, api,_
+from odoo.exceptions import UserError 
+
 
 
 class AccountPayment(models.Model):
@@ -10,3 +12,11 @@ class AccountPayment(models.Model):
     def action_review(self):
         self.sudo().write({'state': 'review'})
 
+    @api.model
+    def create(self, vals):
+        ret = super(AccountPayment, self).create(vals)
+        if not self.user_has_groups('account_security.group_account_customer_payment_create') and ret.payment_type == 'inbound':
+            raise UserError(_("You are not allowed to create"))
+        if not self.user_has_groups('account_security.module_category_accounting_accounting_vendor_payment') and ret.payment_type == 'outbound':
+            raise UserError(_("You are not allowed to create"))
+        return ret
