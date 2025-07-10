f7ba8c6d109fed1d03f4c83c50a4c13c2ccf036a
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-10 13:01:30 +0300

    Add files via upload

---
+++ product_unit_filter/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Product Unit Filter in Sales',
+    'version': '15.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Filter products in sale order based on unit_state',
+    'depends': ['sale', 'product'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
