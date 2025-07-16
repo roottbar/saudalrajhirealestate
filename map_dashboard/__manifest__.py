78488955c3a0623e19526d8c9255bb8dd5ed3ce1
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-16 11:08:29 +0300

    Update __manifest__.py

--- map_dashboard/__manifest__.py
+++ map_dashboard/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Open Map",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "summary": "Add analytic account on products and product categories",
