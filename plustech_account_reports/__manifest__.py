4f398c049339da97efe53431cbd14790d250abd0
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-16 12:50:06 +0300

    Update __manifest__.py

--- plustech_account_reports/__manifest__.py
+++ plustech_account_reports/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Plustech Account Reports',
-    'version': '15.0.1.0.0',
+    'version': '18.0.1.0.0',
     'sequence': 1,
     'depends': [ 'account', 'account_reports', 'account_accountant', 'operating_unit'],
     'data': [
