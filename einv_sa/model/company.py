53be338d88806875fe0bccdddbabf9641a4462e3
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-16 12:34:39 +0300

    Update company.py

--- einv_sa/model/company.py
+++ einv_sa/model/company.py
@@ -8,7 +8,8 @@ from odoo.exceptions import UserError, Warning
 class Company(models.Model):
     _name = "res.company"
     _inherit = "res.company"
-
+    
+    company_registry = fields.Char(string='Company Registry')
     building_no = fields.Char(string="Building no", related='partner_id.building_no', help="Building No")
     district = fields.Char(string="District", related='partner_id.district', help="District")
     code = fields.Char(string="Code", related='partner_id.code', help="Code")
