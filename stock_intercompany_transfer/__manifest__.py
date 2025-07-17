9f37866d7f5f29be8f60b5596b41c0719f8998a1
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-17 07:51:02 +0300

    Update __manifest__.py

--- stock_intercompany_transfer/__manifest__.py
+++ stock_intercompany_transfer/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'Inter Company Stock Transfer',
-    'version': '14.0.1.0.0',
+    'version': '18.0.1.0.0',
     'summary': """Create counterpart Receipt/Delivery Orders between companies.""",
     'description': """Automatically Create Receipt/Delivery orders if any company validates a 
                       Deliver Order/Receipt to the selected company,Inter Company Stock Transfer, Stock Transfer,
