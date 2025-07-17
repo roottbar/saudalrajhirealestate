9180d861f2418b94febda87de740094d4395aafd
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-17 07:55:09 +0300

    Update __manifest__.py

--- tholol_print_payment/__manifest__.py
+++ tholol_print_payment/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Print Payment',
-    'version': '13.0.0.0',
+    'version': '18.0.0.0',
     'category': 'accounting',
     'depends': ['base','account','invoice_templates'],
     'data': [
