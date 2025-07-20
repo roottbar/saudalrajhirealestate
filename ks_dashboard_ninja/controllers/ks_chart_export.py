668600b1b48e7ef5137edb06a92cd819fd727414
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-20 10:28:07 +0300

    Update ks_chart_export.py

--- ks_dashboard_ninja/controllers/ks_chart_export.py
+++ ks_dashboard_ninja/controllers/ks_chart_export.py
@@ -1,16 +1,10 @@
-
-import re
-import datetime
 import io
 import json
 import operator
 
-from odoo.addons.web.controllers.main import ExportFormat,serialize_exception, ExportXlsxWriter
-from odoo.tools.translate import _
 from odoo import http
-from odoo.http import content_disposition, request
-from odoo.tools.misc import xlwt
-from odoo.exceptions import UserError
+from odoo.http import content_disposition, request, serialize_exception
+from odoo.addons.web.controllers.export import ExportXlsxWriter
 from odoo.tools import pycompat
 
 
@@ -18,9 +12,9 @@ class KsChartExport(http.Controller):
 
     def base(self, data):
         params = json.loads(data)
-        header,chart_data = operator.itemgetter('header','chart_data')(params)
+        header, chart_data = operator.itemgetter('header', 'chart_data')(params)
         chart_data = json.loads(chart_data)
-        chart_data['labels'].insert(0,'Measure')
+        chart_data['labels'].insert(0, 'Measure')
         columns_headers = chart_data['labels']
         import_data = []
 
@@ -28,19 +22,17 @@ class KsChartExport(http.Controller):
             dataset['data'].insert(0, dataset['label'])
             import_data.append(dataset['data'])
 
-        return request.make_response(self.from_data(columns_headers, import_data),
-            headers=[('Content-Disposition',
-                            content_disposition(self.filename(header))),
-                     ('Content-Type', self.content_type)],
-            # cookies={'fileToken': token}
-                                     )
+        return request.make_response(
+            self.from_data(columns_headers, import_data),
+            headers=[
+                ('Content-Disposition', content_disposition(self.filename(header))),
+                ('Content-Type', self.content_type)
+            ],
+        )
 
 
+class KsChartExcelExport(KsChartExport):
 
-
-class KsChartExcelExport(KsChartExport, http.Controller):
-
-    # Excel needs raw data to correctly handle numbers and date values
     raw_data = True
 
     @http.route('/ks_dashboard_ninja/export/chart_xls', type='http', auth="user")
@@ -60,11 +52,10 @@ class KsChartExcelExport(KsChartExport, http.Controller):
             for row_index, row in enumerate(rows):
                 for cell_index, cell_value in enumerate(row):
                     xlsx_writer.write_cell(row_index + 1, cell_index, cell_value)
-
         return xlsx_writer.value
 
 
-class KsChartCsvExport(KsChartExport, http.Controller):
+class KsChartCsvExport(KsChartExport):
 
     @http.route('/ks_dashboard_ninja/export/chart_csv', type='http', auth="user")
     @serialize_exception
@@ -83,14 +74,11 @@ class KsChartCsvExport(KsChartExport, http.Controller):
         writer = pycompat.csv_writer(fp, quoting=1)
 
         writer.writerow(fields)
-
         for data in rows:
             row = []
             for d in data:
-                # Spreadsheet apps tend to detect formulas on leading =, + and -
-                if isinstance(d, str)    and d.startswith(('=', '-', '+')):
+                if isinstance(d, str) and d.startswith(('=', '-', '+')):
                     d = "'" + d
-
                 row.append(pycompat.to_text(d))
             writer.writerow(row)
 
