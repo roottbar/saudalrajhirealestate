1512df3748eded86687afc0e233e14c11d08970b
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-20 12:44:11 +0300

    Update ks_dashboard_export.py

--- ks_dashboard_ninja/controllers/ks_dashboard_export.py
+++ ks_dashboard_ninja/controllers/ks_dashboard_export.py
@@ -2,11 +2,9 @@ import io
 import json
 import operator
 
-# from odoo.addons.web.controllers.main import ExportFormat,serialize_exception
-
 from odoo import http
 from odoo.http import request
-from odoo.http import content_disposition,request
+from odoo.http import content_disposition
 
 
 class KsDashboardExport(http.Controller):
@@ -17,15 +15,12 @@ class KsDashboardExport(http.Controller):
         return request.make_response(self.from_data(dashboard_data),
                                      headers=[('Content-Disposition',
                                                content_disposition(self.filename(header))),
-                                              ('Content-Type', self.content_type)],
-                                     # cookies={'fileToken': token}
-                                     )
+                                              ('Content-Type', self.content_type)])
 
 
 class KsDashboardJsonExport(KsDashboardExport, http.Controller):
 
     @http.route('/ks_dashboard_ninja/export/dashboard_json', type='http', auth="user")
-    @serialize_exception
     def index(self, data):
         return self.base(data)
 
@@ -39,13 +34,12 @@ class KsDashboardJsonExport(KsDashboardExport, http.Controller):
     def from_data(self, dashboard_data):
         fp = io.StringIO()
         fp.write(json.dumps(dashboard_data))
-
         return fp.getvalue()
+
 
 class KsItemJsonExport(KsDashboardExport, http.Controller):
 
     @http.route('/ks_dashboard_ninja/export/item_json', type='http', auth="user")
-    @serialize_exception
     def index(self, data):
         data = json.loads(data)
         item_id = data["item_id"]
@@ -63,5 +57,4 @@ class KsItemJsonExport(KsDashboardExport, http.Controller):
     def from_data(self, dashboard_data):
         fp = io.StringIO()
         fp.write(json.dumps(dashboard_data))
-
         return fp.getvalue()
