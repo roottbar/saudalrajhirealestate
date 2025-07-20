321d380743b5fbfb8b893ace5958af280c20ecc0
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-20 12:34:40 +0300

    Update ks_list_export.py

--- ks_dashboard_ninja/controllers/ks_list_export.py
+++ ks_dashboard_ninja/controllers/ks_list_export.py
@@ -8,7 +8,7 @@ import pytz
 import datetime
 
 from odoo import http
-from odoo.http import content_disposition, request, serialize_exception
+from odoo.http import content_disposition, request
 from odoo.tools import pycompat
 from odoo.tools.misc import DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT
 from odoo.addons.web.controllers.export import ExportXlsxWriter
@@ -21,70 +21,78 @@ class KsListExport(http.Controller):
 
     def base(self, data):
         params = json.loads(data)
-        # header,list_data = operator.itemgetter('header','chart_data')(params)
-        header, list_data, item_id, ks_export_boolean, context, params = operator.itemgetter('header', 'chart_data',
-                                                                                             'ks_item_id',
-                                                                                             'ks_export_boolean',
-                                                                                             'context', 'params')(
-            params)
+        header, list_data, item_id, ks_export_boolean, context, params = operator.itemgetter(
+            'header', 'chart_data', 'ks_item_id', 'ks_export_boolean', 'context', 'params'
+        )(params)
+
         list_data = json.loads(list_data)
+
         if ks_export_boolean:
             item = request.env['ks_dashboard_ninja.item'].browse(int(item_id))
             ks_timezone = item._context.get('tz') or item.env.user.tz
+
             if not ks_timezone:
                 ks_tzone = os.environ.get('TZ')
                 if ks_tzone:
                     ks_timezone = ks_tzone
                 elif os.path.exists('/etc/timezone'):
-                    ks_tzone = open('/etc/timezone').read()
-                    ks_timezone = ks_tzone[0:-1]
+                    with open('/etc/timezone') as tz_file:
+                        ks_timezone = tz_file.read().strip()
                     try:
-                        datetime.now(pytz.timezone(ks_timezone))
-                    except Exception as e:
+                        datetime.datetime.now(pytz.timezone(ks_timezone))
+                    except Exception:
                         _logger.info('Please set the local timezone')
-
                 else:
                     _logger.info('Please set the local timezone')
+
             orderby = item.ks_sort_by_field.id
             sort_order = item.ks_sort_by_order
             ks_start_date = context.get('ksDateFilterStartDate', False)
             ks_end_date = context.get('ksDateFilterEndDate', False)
             ksDateFilterSelection = context.get('ksDateFilterSelection', False)
+
             if context.get('allowed_company_ids', False):
                 item = item.with_context(allowed_company_ids=context.get('allowed_company_ids'))
+
             if item.ks_data_calculation_type == 'query':
                 query_start_date = item.ks_query_start_date
                 query_end_date = item.ks_query_end_date
                 ks_query = str(item.ks_custom_query)
+
             if ks_start_date and ks_end_date:
-                ks_start_date = datetime.datetime.strptime(ks_start_date,DEFAULT_SERVER_DATETIME_FORMAT)
-                ks_end_date = datetime.datetime.strptime(ks_end_date,DEFAULT_SERVER_DATETIME_FORMAT)
-            item = item.with_context(ksDateFilterStartDate=ks_start_date)
-            item = item.with_context(ksDateFilterEndDate=ks_end_date)
-            item = item.with_context(ksDateFilterSelection=ksDateFilterSelection)
+                ks_start_date = datetime.datetime.strptime(ks_start_date, DEFAULT_SERVER_DATETIME_FORMAT)
+                ks_end_date = datetime.datetime.strptime(ks_end_date, DEFAULT_SERVER_DATETIME_FORMAT)
+
+            item = item.with_context(ksDateFilterStartDate=ks_start_date,
+                                     ksDateFilterEndDate=ks_end_date,
+                                     ksDateFilterSelection=ksDateFilterSelection)
 
             if item._context.get('ksDateFilterSelection', False):
-                ks_date_filter_selection = item._context['ksDateFilterSelection']
-                if ks_date_filter_selection == 'l_custom':
-                    item = item.with_context(ksDateFilterStartDate=ks_start_date)
-                    item = item.with_context(ksDateFilterEndDate=ks_end_date)
-                    item = item.with_context(ksIsDefultCustomDateFilter=False)
-
+                if ksDateFilterSelection == 'l_custom':
+                    item = item.with_context(
+                        ksDateFilterStartDate=ks_start_date,
+                        ksDateFilterEndDate=ks_end_date,
+                        ksIsDefultCustomDateFilter=False
+                    )
             else:
                 ks_date_filter_selection = item.ks_dashboard_ninja_board_id.ks_date_filter_selection
-                item = item.with_context(ksDateFilterStartDate=item.ks_dashboard_ninja_board_id.ks_dashboard_start_date)
-                item = item.with_context(ksDateFilterEndDate=item.ks_dashboard_ninja_board_id.ks_dashboard_end_date)
-                item = item.with_context(ksDateFilterSelection=ks_date_filter_selection)
-                item = item.with_context(ksIsDefultCustomDateFilter=True)
+                item = item.with_context(
+                    ksDateFilterStartDate=item.ks_dashboard_ninja_board_id.ks_dashboard_start_date,
+                    ksDateFilterEndDate=item.ks_dashboard_ninja_board_id.ks_dashboard_end_date,
+                    ksDateFilterSelection=ks_date_filter_selection,
+                    ksIsDefultCustomDateFilter=True
+                )
 
             if ks_date_filter_selection not in ['l_custom', 'l_none']:
                 ks_date_data = ks_get_date(ks_date_filter_selection, request, 'datetime')
-                item = item.with_context(ksDateFilterStartDate=ks_date_data["selected_start_date"])
-                item = item.with_context(ksDateFilterEndDate=ks_date_data["selected_end_date"])
+                item = item.with_context(
+                    ksDateFilterStartDate=ks_date_data["selected_start_date"],
+                    ksDateFilterEndDate=ks_date_data["selected_end_date"]
+                )
 
             item_domain = params.get('ks_domain_1', [])
-            ks_chart_domain = item.ks_convert_into_proper_domain(item.ks_domain, item,item_domain)
-            # list_data = item.ks_fetch_list_view_data(item,ks_chart_domain, ks_export_all=
+            ks_chart_domain = item.ks_convert_into_proper_domain(item.ks_domain, item, item_domain)
+
             if list_data['type'] == 'ungrouped':
                 list_data = item.ks_fetch_list_view_data(item, ks_chart_domain, ks_export_all=True)
             elif list_data['type'] == 'grouped':
@@ -93,49 +101,53 @@ class KsListExport(http.Controller):
                 if ks_start_date or ks_end_date:
                     query_start_date = ks_start_date
                     query_end_date = ks_end_date
-                ks_query_result = item.ks_get_list_query_result(ks_query, query_start_date, query_end_date, ks_offset=0,
-                                                                ks_export_all=True)
+                ks_query_result = item.ks_get_list_query_result(
+                    ks_query, query_start_date, query_end_date,
+                    ks_offset=0, ks_export_all=True
+                )
                 list_data = item.ks_format_query_result(ks_query_result)
 
-        # chart_data['labels'].insert(0,'Measure')
         columns_headers = list_data['label']
         import_data = []
 
         for dataset in list_data['data_rows']:
-            if not list_data['type'] == 'grouped':
+            if list_data['type'] != 'grouped':
                 for count, index in enumerate(dataset['ks_column_type']):
-                    if index == 'datetime':
-                        ks_converted_date = False
+                    if index == 'datetime' and dataset['data'][count]:
                         date_string = dataset['data'][count]
-                        if dataset['data'][count]:
-                            ks_converted_date = ks_convert_into_local(datetime.datetime.strptime(date_string, '%m/%d/%y %H:%M:%S'),ks_timezone)
+                        ks_converted_date = ks_convert_into_local(
+                            datetime.datetime.strptime(date_string, '%m/%d/%y %H:%M:%S'),
+                            ks_timezone
+                        )
                         dataset['data'][count] = ks_converted_date
+
             for ks_count, val in enumerate(dataset['data']):
                 if isinstance(val, (float, int)):
-                    if val >= 0:
-                        try:
-                            ks_precision = item.sudo().env.ref('ks_dashboard_ninja.ks_dashboard_ninja_precision').digits
-                        except Exception as e:
-                            ks_precision = 2
-                        dataset['data'][ks_count] = item.env['ir.qweb.field.float'].sudo().value_to_html(val,
-                                                                             {'precision': ks_precision})
+                    try:
+                        ks_precision = item.sudo().env.ref(
+                            'ks_dashboard_ninja.ks_dashboard_ninja_precision'
+                        ).digits
+                    except Exception:
+                        ks_precision = 2
+                    dataset['data'][ks_count] = item.env['ir.qweb.field.float'].sudo().value_to_html(
+                        val, {'precision': ks_precision}
+                    )
             import_data.append(dataset['data'])
 
-        return request.make_response(self.from_data(columns_headers, import_data),
-            headers=[('Content-Disposition',
-                            content_disposition(self.filename(header))),
-                     ('Content-Type', self.content_type)],
-            # cookies={'fileToken': token}
-                                     )
+        return request.make_response(
+            self.from_data(columns_headers, import_data),
+            headers=[
+                ('Content-Disposition', content_disposition(self.filename(header))),
+                ('Content-Type', self.content_type)
+            ]
+        )
 
 
 class KsListExcelExport(KsListExport, http.Controller):
 
-    # Excel needs raw data to correctly handle numbers and date values
     raw_data = True
 
     @http.route('/ks_dashboard_ninja/export/list_xls', type='http', auth="user")
-    @serialize_exception
     def index(self, data):
         return self.base(data)
 
@@ -151,14 +163,12 @@ class KsListExcelExport(KsListExport, http.Controller):
             for row_index, row in enumerate(rows):
                 for cell_index, cell_value in enumerate(row):
                     xlsx_writer.write_cell(row_index + 1, cell_index, cell_value)
-
         return xlsx_writer.value
 
 
 class KsListCsvExport(KsListExport, http.Controller):
 
     @http.route('/ks_dashboard_ninja/export/list_csv', type='http', auth="user")
-    @serialize_exception
     def index(self, data):
         return self.base(data)
 
@@ -178,10 +188,8 @@ class KsListCsvExport(KsListExport, http.Controller):
         for data in rows:
             row = []
             for d in data:
-                # Spreadsheet apps tend to detect formulas on leading =, + and -
-                if isinstance(d, str)    and d.startswith(('=', '-', '+')):
+                if isinstance(d, str) and d.startswith(('=', '-', '+')):
                     d = "'" + d
-
                 row.append(pycompat.to_text(d))
             writer.writerow(row)
 
