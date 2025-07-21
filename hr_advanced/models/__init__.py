ae3258d92890c2534391368ad0b36c5e51592c91
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-21 12:05:17 +0300

    Delete hr_advanced directory

--- hr_advanced/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import hr_contract
-from . import hr_employee
-from . import hr_leave_type
-from . import hr_medical_insurance_type
-from . import hr_payslip_line
-from . import employee_documents
