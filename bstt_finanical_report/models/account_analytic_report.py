eb37af7b119b4178ffdce8288124c701f92d2979
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-21 22:14:28 +0300

    Update account_analytic_report.py

--- bstt_finanical_report/models/account_analytic_report.py
+++ bstt_finanical_report/models/account_analytic_report.py
@@ -2,36 +2,39 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
-from odoo.addons.web.controllers.main import clean_action
 
 
-class analytic_report(models.AbstractModel):
+class AnalyticReport(models.AbstractModel):
     _inherit = 'account.analytic.report'
 
     def _get_columns_name(self, options):
-        return [{'name': ''},
-                {'name': _('Reference')},
-                {'name': _('Partner')},
-                {'name': _('Debit'), 'class': 'number'},
-                {'name': _('Credit')},
-                {'name': _('Balance'), 'class': 'number'}]
+        return [
+            {'name': ''},
+            {'name': _('Reference')},
+            {'name': _('Partner')},
+            {'name': _('Debit'), 'class': 'number'},
+            {'name': _('Credit')},
+            {'name': _('Balance'), 'class': 'number'}
+        ]
 
     def _generate_analytic_account_lines(self, analytic_accounts, parent_id=False):
         lines = []
 
         for account in analytic_accounts:
             lines.append({
-                'id': 'analytic_account_%s' % account.id,
+                'id': f'analytic_account_{account.id}',
                 'name': account.name,
-                'columns': [{'name': account.code},
-                            {'name': account.partner_id.display_name},
-                            {'name': self.format_value(account.debit)},
-                            {'name': self.format_value(account.credit)},
-                            {'name': self.format_value(account.balance)}],
-                'level': 4,  # todo check redesign financial reports, should be level + 1 but doesn't look good
+                'columns': [
+                    {'name': account.code},
+                    {'name': account.partner_id.display_name},
+                    {'name': self.format_value(account.debit)},
+                    {'name': self.format_value(account.credit)},
+                    {'name': self.format_value(account.balance)},
+                ],
+                'level': 4,  # يمكن تعديله حسب التصميم الجديد للتقارير المالية
                 'unfoldable': False,
                 'caret_options': 'account.analytic.account',
-                'parent_id': parent_id,  # to make these fold when the original parent gets folded
+                'parent_id': parent_id,
             })
 
         return lines
@@ -39,19 +42,21 @@ class analytic_report(models.AbstractModel):
     def _generate_analytic_group_line(self, group, analytic_line_domain, unfolded=False):
         LOWEST_LEVEL = 1
         balance = self._get_balance_for_group(group, analytic_line_domain)
-        print(balance)
 
-        accounts = self.env['account.analytic.account'].search([('group_id', 'child_of', group.id)])
-        debits = sum([row['debit'] for row in accounts])
-        credits = sum([row['credit'] for row in accounts])
+        accounts = self.env['account.analytic.account'].search([
+            ('group_id', 'child_of', group.id)
+        ])
+        debits = sum(account.debit for account in accounts)
+        credits = sum(account.credit for account in accounts)
 
         line = {
-            'columns': [{'name': ''},
-                        {'name': ''},
-                        {'name': self.format_value(debits)},
-                        {'name': self.format_value(credits)},
-                        {'name': self.format_value(balance)},
-                        ],
+            'columns': [
+                {'name': ''},
+                {'name': ''},
+                {'name': self.format_value(debits)},
+                {'name': self.format_value(credits)},
+                {'name': self.format_value(balance)},
+            ],
             'unfoldable': True,
             'unfolded': unfolded,
         }
@@ -61,7 +66,7 @@ class analytic_report(models.AbstractModel):
                 'id': group.id,
                 'name': group.name,
                 'level': LOWEST_LEVEL + self._get_amount_of_parents(group),
-                'parent_id': group.parent_id.id,  # to make these fold when the original parent gets folded
+                'parent_id': group.parent_id.id,
             })
         else:
             line.update({
