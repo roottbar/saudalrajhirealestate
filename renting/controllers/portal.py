320d5163b72dc3347d4357ef7d14962b81e2ccc1
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-21 12:36:57 +0300

    Update portal.py

--- renting/controllers/portal.py
+++ renting/controllers/portal.py
@@ -1,23 +1,20 @@
+# -*- coding: utf-8 -*-
+import base64
 import binascii
 
 from odoo import fields, http, SUPERUSER_ID, _
 from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
-# from odoo.addons.payment.controllers.portal import PaymentProcessing
-from odoo.addons.portal.controllers.mail import _message_post_helper
-from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
-from odoo.osv import expression
-from odoo import http
-from odoo.addons.sale.controllers.portal import CustomerPortal  # Import the class
+from odoo.addons.portal.controllers.portal import CustomerPortal
 
 
-class CustomCustomerPortal(CustomerPortal):  # Inherit in your custom class
+class CustomCustomerPortal(CustomerPortal):
 
     @http.route(['/my/orders/<int:order_id>/accept'], type='json', auth="public", website=True)
     def portal_quote_accept(self, order_id, access_token=None, name=None, signature=None):
-        # get from query string if not on json param
         res = super(CustomCustomerPortal, self).portal_quote_accept(order_id)
         access_token = access_token or request.httprequest.args.get('access_token')
+
         try:
             order_sudo = self._document_check_access('sale.order', order_id, access_token=access_token)
         except (AccessError, MissingError):
@@ -40,17 +37,29 @@ class CustomCustomerPortal(CustomerPortal):  # Inherit in your custom class
             order_sudo.action_confirm()
             order_sudo._send_order_confirmation_mail()
 
-        pdf = request.env.ref('sale.action_report_saleorder').with_user(SUPERUSER_ID)._render_qweb_pdf([order_sudo.id])[
-            0]
+        # Generate the PDF
+        pdf = request.env.ref('sale.action_report_saleorder').with_user(SUPERUSER_ID)._render_qweb_pdf([order_sudo.id])[0]
 
-        _message_post_helper(
-            'sale.order', order_sudo.id, _('Order signed by %s') % (name,),
-            attachments=[('%s.pdf' % order_sudo.name, pdf)],
-            **({'token': access_token} if access_token else {}))
+        # Create the attachment
+        attachment = request.env['ir.attachment'].sudo().create({
+            'name': '%s.pdf' % order_sudo.name,
+            'datas': base64.b64encode(pdf),
+            'res_model': 'sale.order',
+            'res_id': order_sudo.id,
+            'type': 'binary',
+            'mimetype': 'application/pdf',
+        })
+
+        # Post message with attachment
+        order_sudo.message_post(
+            body=_('Order signed by %s') % (name,),
+            attachment_ids=[attachment.id],
+        )
 
         query_string = '&message=sign_ok'
         if order_sudo.has_to_be_paid(True):
             query_string += '#allow_payment=yes'
+
         return {
             'force_refresh': True,
             'redirect_url': order_sudo.get_portal_url(query_string=query_string),
