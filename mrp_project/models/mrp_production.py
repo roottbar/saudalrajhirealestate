bc4d2714fdefe767725220fa2870881b973d545b
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-27 15:12:17 +0300

    Update mrp_production.py

--- mrp_project/models/mrp_production.py
+++ mrp_project/models/mrp_production.py
@@ -1,89 +1,98 @@
 # -*- coding: utf-8 -*-
-# (c) 2014 Daniel Campos <danielcampos@avanzosc.es>
-# (c) 2015 Pedro M. Baeza - Serv. Tecnol. Avanzados
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields, api, _
-
+from odoo import api, fields, models
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     project_id = fields.Many2one(
-        comodel_name="project.project", string="Project", copy=False,
-        readonly=True, states={'draft': [('readonly', False)]})
+        'project.project', 'Project',
+        help="Project to which this production order is linked."
+    )
     analytic_account_id = fields.Many2one(
-        related="project_id.analytic_account_id", store=True)
+        related='project_id.analytic_account_id',
+        string='Analytic Account',
+        store=True,
+        readonly=True
+    )
+    
+    partner_id = fields.Many2one(
+        'res.partner', 
+        string='Customer',
+        compute='_compute_partner_id',
+        store=True,
+        help="Customer from the related sales order"
+    )
+    
+    @api.depends('origin')
+    def _compute_partner_id(self):
+        for production in self:
+            partner_id = False
+            if production.origin:
+                # البحث عن أمر البيع المرتبط
+                sale_order = self.env['sale.order'].search([
+                    ('name', '=', production.origin)
+                ], limit=1)
+                if sale_order:
+                    partner_id = sale_order.partner_id.id
+            production.partner_id = partner_id
 
-    @api.model
-    def _prepare_project_vals(self, production):
-        parent_id = False
-        if 'sale_id' in production._fields:
-            parent_id = production.sale_id.project_id.id
+    def _prepare_project_values(self):
         return {
-            'name': production.name,
-            'automatic_creation': True,
-            'parent_id': parent_id,
+            'name': self.name,
+            'partner_id': self.partner_id.id if self.partner_id else False,
+            'user_id': self.user_id.id,
+            'company_id': self.company_id.id,
+        }
+
+    def _prepare_production_task_values(self, project):
+        return {
+            'name': '%s: %s' % (self.name, self.product_id.name),
+            'project_id': project.id,
+            'user_id': self.user_id.id,
+            'partner_id': self.partner_id.id if self.partner_id else False,
+            'company_id': self.company_id.id,
+            'description': 'Manufacturing Order: %s' % self.name,
         }
 
     @api.model
-    def _prepare_production_task(self, production):
-        product = production.product_id
-        task_name = "%s::%s%s" % (
-            production.name,
-            "[%s] " % product.default_code if product.default_code else "",
-            product.name)
-        task_descr = _("""Manufacturing Order: %s
-Product to Produce: [%s]%s
-Quantity to Produce: %s
-Bill of Material: %s
-Planned Date: %s""") % (
-            production.name, production.product_id.default_code,
-            production.product_id.name, production.product_qty,
-            production.bom_id.name, production.date_planned_start)
-        return {
-            'mrp_production_id': production.id,
-            'user_ids': [(6, 0, [production.user_id.id])] if production.user_id else [],
-            'name': task_name,
-            'project_id': production.project_id.id,
-            'description': task_descr
-        }
+    def create(self, vals):
+        production = super(MrpProduction, self).create(vals)
+        if production.project_id:
+            production._create_production_task()
+        return production
+
+    def write(self, vals):
+        result = super(MrpProduction, self).write(vals)
+        if 'project_id' in vals:
+            for production in self:
+                if production.project_id:
+                    production._create_production_task()
+        return result
+
+    def _create_production_task(self):
+        if self.project_id:
+            task_vals = self._prepare_production_task_values(self.project_id)
+            task = self.env['project.task'].create(task_vals)
+            return task
+        return False
 
     def action_confirm(self):
-        project_obj = self.env['project.project']
         result = super(MrpProduction, self).action_confirm()
         for production in self:
-            if not production.project_id:
-                project_vals = self._prepare_project_vals(production)
-                project = project_obj.create(project_vals)
+            if not production.project_id and production.analytic_account_id:
+                # إنشاء مشروع تلقائياً إذا كان هناك حساب تحليلي
+                project_vals = production._prepare_project_values()
+                project_vals['analytic_account_id'] = production.analytic_account_id.id
+                project = self.env['project.project'].create(project_vals)
                 production.project_id = project.id
-            # تحديث analytic_account_id في stock moves
-            if production.analytic_account_id:
-                production.move_raw_ids.write({
-                    'analytic_account_id': production.analytic_account_id.id
-                })
-                production.move_finished_ids.write({
-                    'analytic_account_id': production.analytic_account_id.id
-                })
+                production._create_production_task()
         return result
 
     def unlink(self):
-        projects = self.mapped('project_id').filtered('automatic_creation')
-        tasks = self.env['project.task'].search(
-            [('project_id', 'in', projects.ids)])
-        if not tasks.mapped('timesheet_ids'):
-            child_tasks = tasks.filtered(lambda x: x.child_ids)
-            child_tasks.unlink()
-            (tasks - child_tasks).unlink()
-            projects.unlink()
+        # حذف المهام المرتبطة
+        tasks = self.env['project.task'].search([
+            ('name', 'like', self.name)
+        ])
+        tasks.unlink()
         return super(MrpProduction, self).unlink()
-
-
-class MrpWorkorder(models.Model):
-    _inherit = 'mrp.workorder'
-
-    task_ids = fields.One2many(
-        comodel_name="project.task", inverse_name="workorder", string="Tasks")
-    timesheet_ids = fields.One2many(
-        comodel_name="account.analytic.line", inverse_name="workorder",
-        string="Timesheets")
