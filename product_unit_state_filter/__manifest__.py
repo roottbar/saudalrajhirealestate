2647f6b7e960b1afd212351813a1c73593256fb9
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-27 11:27:16 +0300

    Add files via upload

---
+++ product_unit_state_filter/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Filter Products by Unit State",
+    "version": "15.0.1.0.0",
+    "category": "Sales",
+    "summary": "Hide products not marked as 'شاغرة' in sale order lines",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_line_view.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
