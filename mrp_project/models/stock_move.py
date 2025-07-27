4866565973349d235b5bef7393d7f111db3ca454
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-27 14:10:53 +0300

    Update stock_move.py

--- mrp_project/models/stock_move.py
+++ mrp_project/models/stock_move.py
@@ -11,6 +11,16 @@ class StockMove(models.Model):
     workorder_id = fields.Many2one(
         comodel_name='mrp.workorder',
         string='Work Order',
-        related='workorder_ids.workorder_id',
+        compute='_compute_workorder_id',
         store=False
     )
+
+    def _compute_workorder_id(self):
+        for move in self:
+            workorder = False
+            if move.raw_material_production_id:
+                # For raw materials, find the workorder from the production
+                workorders = move.raw_material_production_id.workorder_ids
+                if workorders:
+                    workorder = workorders[0]  # Take the first workorder
+            move.workorder_id = workorder
