6613017d731f66d1c2bd306be1b7969feb6fec43
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-27 15:11:25 +0300

    Update mrp_production.py

--- mrp_analytic/models/mrp_production.py
+++ mrp_analytic/models/mrp_production.py
@@ -1,19 +1,45 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent - Jordi Ballester Alomar
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
-
+from odoo import api, fields, models
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    partner_id = fields.Many2one(
-        comodel_name='res.partner', 
-        string='Customer',
-        help='Customer for this manufacturing order'
-    )
     analytic_account_id = fields.Many2one(
-        comodel_name='account.analytic.account', 
-        string='Analytic Account'
+        'account.analytic.account', 'Analytic Account',
+        domain="[('company_id', '=', company_id)]",
+        help="Analytic account to which this production order is linked for analytical purposes."
     )
+    
+    partner_id = fields.Many2one(
+        'res.partner', 
+        string='Customer',
+        compute='_compute_partner_id',
+        store=True,
+        help="Customer from the related sales order"
+    )
+    
+    @api.depends('origin')
+    def _compute_partner_id(self):
+        for production in self:
+            partner_id = False
+            if production.origin:
+                # البحث عن أمر البيع المرتبط
+                sale_order = self.env['sale.order'].search([
+                    ('name', '=', production.origin)
+                ], limit=1)
+                if sale_order:
+                    partner_id = sale_order.partner_id.id
+            production.partner_id = partner_id
+
+    def _prepare_analytic_line_values(self, account_id, amount, unit_amount, description):
+        return {
+            'name': description,
+            'amount': amount,
+            'unit_amount': unit_amount,
+            'account_id': account_id,
+            'partner_id': self.partner_id.id if self.partner_id else False,
+            'user_id': self.env.user.id,
+            'date': fields.Date.today(),
+            'company_id': self.company_id.id,
+        }
