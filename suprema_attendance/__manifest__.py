48df000a3670f1f33f805278f668a2392994c69e
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-28 14:07:58 +0300

    Update __manifest__.py

--- suprema_attendance/__manifest__.py
+++ suprema_attendance/__manifest__.py
@@ -9,7 +9,7 @@
     'author': 'Your Company',
     'website': 'https://www.yourcompany.com',
     'category': 'Human Resources',
-    'depends': ['hr', 'hr_attendance'],
+    'depends': ['web', 'hr', 'hr_attendance'],
     'data': [
         'security/ir.model.access.csv',
         'security/security_rules.xml',
@@ -18,6 +18,11 @@
         'views/templates.xml',
         'views/menu.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'suprema_attendance/static/src/js/suprema_connection.js',
+        ],
+    },
     'external_dependencies': {
         'python': ['pyzk'],
     },
