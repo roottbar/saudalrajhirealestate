eb314a48e504ee8d9d0da125234c13cbbcedde0e
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-07-30 11:22:53 +0300

    Update cost_center_report.py

--- cost_center_reports/models/cost_center_report.py
+++ cost_center_reports/models/cost_center_report.py
@@ -1,25 +1,20 @@
 from odoo import models, fields, api
-from odoo.tools import date_utils
-from datetime import date, datetime
+from datetime import date
 
 class CostCenterReport(models.TransientModel):
     _name = 'cost.center.report'
     _description = 'Cost Center Report'
 
-     date_from = fields.Date(
+    date_from = fields.Date(
         string='From Date',
         required=True,
-        default=lambda self: self._default_date_from())
+        default=lambda self: fields.Date.context_today(self).replace(day=1)
+    )
     date_to = fields.Date(
         string='To Date',
         required=True,
-        default=lambda self: self._default_date_to())
-    
-    def _default_date_from(self):
-        return fields.Date.context_today(self).replace(day=1)
-    
-    def _default_date_to(self):
-        return fields.Date.context_today(self)
+        default=lambda self: fields.Date.context_today(self)
+    )
     group_id = fields.Many2one('account.analytic.group', string='Analytic Group')
     analytic_account_ids = fields.Many2many(
         'account.analytic.account',
