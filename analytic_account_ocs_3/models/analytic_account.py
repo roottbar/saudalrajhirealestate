1dc62fbb1fe32fa4687858382c8d7b8cc7369c26
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-03 11:00:52 +0300

    Update analytic_account.py

--- analytic_account_ocs_3/models/analytic_account.py
+++ analytic_account_ocs_3/models/analytic_account.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 class AccountAnalyticAccount(models.Model):
     _inherit = 'account.analytic.account'
@@ -8,5 +8,13 @@ class AccountAnalyticAccount(models.Model):
     operating_unit_id = fields.Many2one(
         'operating.unit',
         string='Operating Unit',
-        domain="[('company_id', '=', company_id)]"
+        domain="[('company_id', '=', company_id)]",
+        help="Operating unit for this analytic account"
     )
+    
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        """Override name search to include operating unit filtering"""
+        if args is None:
+            args = []
+        return super()._name_search(name, args, operator, limit, name_get_uid)
