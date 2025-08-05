4dae456dd3fd3cf2cb214d615c71088a76d0c5ae
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-05 23:10:59 +0300

    Create property.py

---
+++ rental_reports_ocs/models/property.py
@@ -0,0 +1,21 @@
+from odoo import models, fields, api
+
+class RentProperty(models.Model):
+    _name = 'rent.property'
+    _description = 'Rental Property'
+
+    name = fields.Char(string='Property Name', required=True)
+    address = fields.Text(string='Address')
+    operating_unit_id = fields.Many2one('operating.unit', string='Operating Unit')
+    elevators = fields.One2many('rent.property.elevator', 'property_id', string='Elevators')
+
+class RentPropertyElevator(models.Model):
+    _name = 'rent.property.elevator'
+    _description = 'Property Elevator'
+    _rec_name = 'elevator_maintenance_date'
+
+    property_id = fields.Many2one('rent.property', copy=True, string='Properties', ondelete='cascade')
+    elevator_maintenance_description = fields.Char(string='Maintenance Description')
+    elevator_number_name = fields.Char(string='Elevator Number/Name')
+    elevator_maintenance_date = fields.Date(string='Maintenance Date')
+    elevator_maintenance_value = fields.Float(string='Maintenance Value')
