755f07af112ba87b46cbd2f2c136e348c79b042d
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-05 23:11:55 +0300

    Update rental_report.py

--- rental_reports_ocs/models/rental_report.py
+++ rental_reports_ocs/models/rental_report.py
@@ -6,15 +6,13 @@ class RentalReportWizard(models.TransientModel):
 
     company_id = fields.Many2one('res.company', string='Company', required=True)
     operating_unit_id = fields.Many2one('operating.unit', string='Operating Unit')
-    property_address_build = fields.Char(string='Building Address')
-    property_id = fields.Many2one('property.property', string='Property')
+    property_id = fields.Many2one('rent.property', string='Property')
     
     def generate_report(self):
         data = {
             'company_id': self.company_id.id,
             'operating_unit_id': self.operating_unit_id.id,
-            'property_address_build': self.property_address_build,
             'property_id': self.property_id.id,
             'form_data': self.read()[0],
         }
-        return self.env.ref('rental_reports.action_rental_report').report_action(self, data=data)
+        return self.env.ref('rental_reports_ocs.action_rental_report').report_action(self, data=data)
