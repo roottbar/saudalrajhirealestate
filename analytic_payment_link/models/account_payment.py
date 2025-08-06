4e1f84f6f9d07be152959c4dd68e4944c5e9d507
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-06 13:51:16 +0300

    Update account_payment.py

--- analytic_payment_link/models/account_payment.py
+++ analytic_payment_link/models/account_payment.py
@@ -9,36 +9,13 @@ class AccountPayment(models.Model):
         readonly=True,
         states={'draft': [('readonly', False)]}
     )
+    
     analytic_tag_ids = fields.Many2many(
         'account.analytic.tag',
         string='الوسوم التحليلية',
         readonly=True,
         states={'draft': [('readonly', False)]}
     )
-    
-    def _prepare_move_line_default_vals(self, write_off_line_vals=None):
-        """Override to include analytic account and tags in move lines"""
-        res = super(AccountPayment, self)._prepare_move_line_default_vals(write_off_line_vals)
-        
-        # تطبيق المراكز التحليلية على بنود القيد
-        for line in res:
-            if self.analytic_account_id:
-                line['analytic_account_id'] = self.analytic_account_id.id
-            if self.analytic_tag_ids:
-                line['analytic_tag_ids'] = [(6, 0, self.analytic_tag_ids.ids)]
-        
-        # ربط الدفع بالفواتير المحددة
-        if self.payment_invoice_line_ids:
-            for inv_line in self.payment_invoice_line_ids:
-                for move_line in res:
-                    if move_line.get('credit') > 0:  # بند الدفع
-                        move_line['invoice_id'] = inv_line.invoice_id.id
-                        if inv_line.analytic_account_id:
-                            move_line['analytic_account_id'] = inv_line.analytic_account_id.id
-                        if inv_line.analytic_tag_ids:
-                            move_line['analytic_tag_ids'] = [(6, 0, inv_line.analytic_tag_ids.ids)]
-        
-        return res
     
     sale_order_id = fields.Many2one(
         'sale.order', 
@@ -52,7 +29,8 @@ class AccountPayment(models.Model):
             domain = [
                 ('partner_id', '=', payment.partner_id.id),
                 ('state', '=', 'posted'),
-                ('payment_state', '!=', 'paid')
+                ('payment_state', '!=', 'paid'),
+                ('move_type', 'in', ['out_invoice', 'out_refund'])
             ]
             if payment.sale_order_id:
                 domain.append(('invoice_origin', '=', payment.sale_order_id.name))
@@ -71,17 +49,26 @@ class AccountPayment(models.Model):
         string='فواتير الدفع'
     )
     
-    @api.onchange('amount', 'payment_invoice_line_ids')
-    def _onchange_amount_distribution(self):
-        if self.amount and self.payment_invoice_line_ids:
-            total_amount = sum(line.amount for line in self.payment_invoice_line_ids)
-            if total_amount != self.amount:
-                # توزيع المبلغ المتبقي على الفواتير
-                remaining = self.amount - total_amount
-                if remaining > 0 and len(self.payment_invoice_line_ids) > 0:
-                    per_invoice = remaining / len(self.payment_invoice_line_ids)
-                    for line in self.payment_invoice_line_ids:
-                        line.amount += per_invoice
+    def _prepare_move_line_default_vals(self, write_off_line_vals=None):
+        res = super(AccountPayment, self)._prepare_move_line_default_vals(write_off_line_vals)
+        
+        for line in res:
+            if self.analytic_account_id:
+                line['analytic_account_id'] = self.analytic_account_id.id
+            if self.analytic_tag_ids:
+                line['analytic_tag_ids'] = [(6, 0, self.analytic_tag_ids.ids)]
+        
+        if self.payment_invoice_line_ids:
+            for inv_line in self.payment_invoice_line_ids:
+                for move_line in res:
+                    if move_line.get('credit') > 0:  # بند الدفع
+                        move_line['invoice_id'] = inv_line.invoice_id.id
+                        if inv_line.analytic_account_id:
+                            move_line['analytic_account_id'] = inv_line.analytic_account_id.id
+                        if inv_line.analytic_tag_ids:
+                            move_line['analytic_tag_ids'] = [(6, 0, inv_line.analytic_tag_ids.ids)]
+        
+        return res
 
 class PaymentInvoiceLine(models.Model):
     _name = 'payment.invoice.line'
@@ -95,28 +82,30 @@ class PaymentInvoiceLine(models.Model):
         string='الفاتورة',
         required=True,
         domain="[('id', 'in', parent.available_invoice_ids)]"
-    )    available_invoice_ids = fields.Many2many(
-        related='payment_id.available_invoice_ids',
-        string='الفواتير المتاحة'
     )
+    
     analytic_account_id = fields.Many2one(
         'account.analytic.account', 
         string='مركز التكلفة'
     )
+    
     analytic_tag_ids = fields.Many2many(
         'account.analytic.tag', 
         string='الوسوم التحليلية'
     )
+    
     amount = fields.Monetary(
         string='المبلغ', 
         currency_field='currency_id',
         readonly=True
     )
+    
     currency_id = fields.Many2one(
         related='payment_id.currency_id', 
         store=True, 
         readonly=True
     )
+    
     invoice_amount_residual = fields.Monetary(
         related='invoice_id.amount_residual',
         string='المبلغ المتبقي في الفاتورة',
@@ -126,9 +115,7 @@ class PaymentInvoiceLine(models.Model):
     @api.onchange('invoice_id')
     def _onchange_invoice_id(self):
         if self.invoice_id:
-            # تعيين المبلغ التلقائي عند اختيار الفاتورة
             self.amount = min(self.invoice_id.amount_residual, self.payment_id.amount)
-            # تعيين المراكز التحليلية من الفاتورة
             if self.invoice_id.analytic_account_id:
                 self.analytic_account_id = self.invoice_id.analytic_account_id
             if self.invoice_id.analytic_tag_ids:
