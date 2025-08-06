c07fdd011bc99f729f6bf3598173204b643e491a
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-06 10:29:13 +0300

    Update ks_odoo_base.py

--- ks_dashboard_ninja/models/ks_odoo_base.py
+++ ks_dashboard_ninja/models/ks_odoo_base.py
@@ -36,28 +36,3 @@ class Base(models.AbstractModel):
                 ] for partner_id in online_partner]
                 self.env['bus.bus']._sendmany(updates)
         return recs
-
-
-class KSOdooBase(models.Model):
-    _name = 'ks_odoo_base'
-    
-    @api.depends('related_model.field_name', recursive=True)
-    def _compute_analytics(self):
-        self.some_field = self.related_model.field_name
-    
-    @api.depends('cross_model.relation_field', recursive=True)
-    def _compute_metrics(self):
-        # Calculation logic
-    
-    @api.depends('dependent_field', recursive=True)  # Add recursive parameter
-    def _compute_some_field(self):
-        # Your computation logic
-    
-    @api.depends('analytic_account_id.balance', recursive=True)
-    def _compute_financial_metrics(self):
-        for record in self:
-            record.financial_level = record.analytic_account_id.balance
-    
-    @api.depends('report_line_ids.value', recursive=True)
-    def _compute_report_totals(self):
-        self.total_value = sum(line.value for line in self.report_line_ids)
