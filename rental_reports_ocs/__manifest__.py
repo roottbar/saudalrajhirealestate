ace108b0f2937aee426a488ba5e50aba9de3b2d0
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-06 10:06:20 +0300

    Update __manifest__.py

--- rental_reports_ocs/__manifest__.py
+++ rental_reports_ocs/__manifest__.py
@@ -9,7 +9,7 @@
     'author': 'Othmancs',
     'website': 'https://www.tbarholding.com',
     'category': 'Real Estate/Rental',
-    'depends': ['base', 'product', 'operating_unit', 'analytic'],
+    'depends': ['product', 'operating_unit', 'renting', 'rent_customize'],
     'data': [
         'views/property_views.xml',
         'views/rental_report_views.xml',
