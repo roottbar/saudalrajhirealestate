57e3771de67b074fcf6191ee79ec92d34d40da02
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-10 10:42:20 +0300

    Update __manifest__.py

--- project_scrum/__manifest__.py
+++ project_scrum/__manifest__.py
@@ -1,6 +1,3 @@
-# Copyright 2024 Binhex - Adasat Torres de León (https://www.binhex.cloud)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 {
     "name": "Project Scrum",
     "version": "15.0.1.0.0",
