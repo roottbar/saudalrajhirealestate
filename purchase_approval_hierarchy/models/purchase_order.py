559cb1d6f925832376ea31ae9c3f2eaab6a38dab
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-12 14:26:57 +0300

    Update purchase_order.py

--- purchase_approval_hierarchy/models/purchase_order.py
+++ purchase_approval_hierarchy/models/purchase_order.py
@@ -9,15 +9,22 @@ class PurchaseOrder(models.Model):
         ('approval_2', 'Approval Stage 2'),
         ('approval_3', 'Approval Stage 3'),
         ('approved', 'Approved'),
-    ], ondelete={'approval_1': 'set default', 'approval_2': 'set default', 'approval_3': 'set default', 'approved': 'set default'})
+    ], ondelete={
+        'approval_1': 'set default',
+        'approval_2': 'set default',
+        'approval_3': 'set default',
+        'approved': 'set default'
+    })
 
+    @api.returns('self', lambda value: value.id)
     def action_approve_1(self):
-        self.ensure_one()
-        if not self.env.user.has_group('purchase_approval_hierarchy.group_purchase_approval_1'):
-            raise UserError(_("You do not have permission to perform Approval 1."))
-        if self.state != 'draft':
-            raise UserError(_("Order is not in a state to perform Approval 1."))
-        self.state = 'approval_1'
+        for order in self:
+            if not self.env.user.has_group('purchase_approval_hierarchy.group_purchase_approval_1'):
+                raise UserError(_("You don't have the approval level 1 access rights."))
+            if order.state != 'draft':
+                raise UserError(_("Order must be in draft state."))
+            order.write({'state': 'approval_1'})
+        return True
 
     def action_approve_2(self):
         self.ensure_one()
