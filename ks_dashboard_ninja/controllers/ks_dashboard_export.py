fb9f4009bd1612cd689261a4b48e5ca3e5e746de
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-18 14:30:57 +0300

    Update ks_dashboard_export.py

--- ks_dashboard_ninja/controllers/ks_dashboard_export.py
+++ ks_dashboard_ninja/controllers/ks_dashboard_export.py
@@ -1,33 +1,48 @@
 import io
 import json
 import operator
-
-from odoo.addons.web.controllers.main import ExportFormat,serialize_exception
+import logging
 
 from odoo import http
-from odoo.http import request
-from odoo.http import content_disposition,request
+from odoo.http import content_disposition, request
+
+_logger = logging.getLogger(__name__)
 
 
 class KsDashboardExport(http.Controller):
-
     def base(self, data):
         params = json.loads(data)
         header, dashboard_data = operator.itemgetter('header', 'dashboard_data')(params)
-        return request.make_response(self.from_data(dashboard_data),
-                                     headers=[('Content-Disposition',
-                                               content_disposition(self.filename(header))),
-                                              ('Content-Type', self.content_type)],
-                                     # cookies={'fileToken': token}
-                                     )
+        return request.make_response(
+            self.from_data(dashboard_data),
+            headers=[
+                ('Content-Disposition', content_disposition(self.filename(header))),
+                ('Content-Type', self.content_type)
+            ]
+        )
 
 
 class KsDashboardJsonExport(KsDashboardExport, http.Controller):
-
     @http.route('/ks_dashboard_ninja/export/dashboard_json', type='http', auth="user")
-    @serialize_exception
     def index(self, data):
-        return self.base(data)
+        try:
+            return self.base(data)
+        except Exception as e:
+            _logger.exception("Dashboard export failed")
+            return request.make_response(
+                json.dumps({
+                    'code': 200,
+                    'message': "Odoo Server Error",
+                    'data': {
+                        'name': str(e),
+                        'debug': '',
+                        'message': str(e),
+                        'arguments': [],
+                        'context': {}
+                    }
+                }),
+                headers=[('Content-Type', 'application/json')]
+            )
 
     @property
     def content_type(self):
@@ -37,21 +52,33 @@ class KsDashboardJsonExport(KsDashboardExport, http.Controller):
         return base + '.json'
 
     def from_data(self, dashboard_data):
-        fp = io.StringIO()
-        fp.write(json.dumps(dashboard_data))
+        return json.dumps(dashboard_data)
 
-        return fp.getvalue()
 
 class KsItemJsonExport(KsDashboardExport, http.Controller):
-
     @http.route('/ks_dashboard_ninja/export/item_json', type='http', auth="user")
-    @serialize_exception
     def index(self, data):
-        data = json.loads(data)
-        item_id = data["item_id"]
-        data['dashboard_data'] = request.env['ks_dashboard_ninja.board'].ks_export_item(item_id)
-        data = json.dumps(data)
-        return self.base(data)
+        try:
+            data = json.loads(data)
+            item_id = data["item_id"]
+            data['dashboard_data'] = request.env['ks_dashboard_ninja.board'].ks_export_item(item_id)
+            return self.base(json.dumps(data))
+        except Exception as e:
+            _logger.exception("Item export failed")
+            return request.make_response(
+                json.dumps({
+                    'code': 200,
+                    'message': "Odoo Server Error",
+                    'data': {
+                        'name': str(e),
+                        'debug': '',
+                        'message': str(e),
+                        'arguments': [],
+                        'context': {}
+                    }
+                }),
+                headers=[('Content-Type', 'application/json')]
+            )
 
     @property
     def content_type(self):
@@ -61,7 +88,4 @@ class KsItemJsonExport(KsDashboardExport, http.Controller):
         return base + '.json'
 
     def from_data(self, dashboard_data):
-        fp = io.StringIO()
-        fp.write(json.dumps(dashboard_data))
-
-        return fp.getvalue()
+        return json.dumps(dashboard_data)
