b96844ea7e82e878a7e7beee0072a126d44927d5
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-19 11:43:18 +0300

    Update main.py

--- account_dynamic_reports/controllers/main.py
+++ account_dynamic_reports/controllers/main.py
@@ -5,29 +5,29 @@ from odoo import http
 from odoo.http import request
 from odoo.api import Environment
 from odoo import SUPERUSER_ID
-import datetime
-import json
-import logging
-from datetime import datetime
-_logger = logging.getLogger(__name__)
-from odoo.addons.web.controllers.main import serialize_exception, content_disposition
-from odoo.tools.translate import _
 import base64
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class Binary(http.Controller):
     """Common controller to download file"""
 
     @http.route('/web/binary/download_document', type='http', auth="public")
-    @serialize_exception
     def download_document(self, model, field, id, filename=None, **kw):
-        env = Environment(request.cr, SUPERUSER_ID, {})
-        res = env[str(model)].search([('id', '=', int(id))]).sudo().read()[0]
-        filecontent = base64.b64decode(res.get(field) or '')
-        if not filename:
-            filename = '%s_%s' % (model.replace('.', '_'), id)
-        if not filecontent:
+        try:
+            env = Environment(request.cr, SUPERUSER_ID, {})
+            res = env[str(model)].sudo().browse(int(id))
+            filecontent = res[field] and base64.b64decode(res[field]) or b''
+            if not filename:
+                filename = '%s_%s' % (model.replace('.', '_'), id)
+            if not filecontent:
+                return request.not_found()
+            headers = [
+                ('Content-Type', 'application/octet-stream'),
+                ('Content-Disposition', 'attachment; filename="%s"' % filename)
+            ]
+            return request.make_response(filecontent, headers)
+        except Exception as e:
+            _logger.exception("Failed to download document: %s", e)
             return request.not_found()
-        return request.make_response(filecontent,
-                                     [('Content-Type', 'application/octet-stream'),
-                                      ('Content-Disposition', content_disposition(filename))])
