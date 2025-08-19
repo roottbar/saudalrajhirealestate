ded06c01631f690fb806ca5957f26f0cfe6b14c6
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-19 10:07:51 +0300

    Delete customer_tickets directory

--- customer_tickets/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import customer_ticket
-from . import subscription
-from . import sub_ticket_type
-from . import mail_thread
-from . import feedback
-from . import ir_attachment
