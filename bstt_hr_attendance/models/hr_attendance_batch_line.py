fb65541381794df50c5dec4dbb5dff5c16b74256
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-21 07:52:01 +0300

    Update hr_attendance_batch_line.py

--- bstt_hr_attendance/models/hr_attendance_batch_line.py
+++ bstt_hr_attendance/models/hr_attendance_batch_line.py
@@ -2,12 +2,13 @@
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
 from pytz import timezone, UTC, utc
-from odoo.tools import format_datetime
-from odoo.tools import format_time
+from odoo.tools import format_datetime, format_time
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 from odoo.osv import expression
-from odoo.addons.resource.models.resource import float_to_time, HOURS_PER_DAY
+from odoo.addons.resource.models.utils import float_to_time  # ✅ المسار الجديد
+
+HOURS_PER_DAY = 8.0  # أو احذف إذا بتجيب من calendar
 
 
 class HrAttendanceBatchLine(models.Model):
