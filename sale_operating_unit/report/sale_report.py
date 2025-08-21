a8585a42806bf0b81542d934c28d3ef87eaf1eea
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-21 12:54:06 +0300

    Update sale_report.py

--- sale_operating_unit/report/sale_report.py
+++ sale_operating_unit/report/sale_report.py
@@ -1,20 +1,18 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
-
 from odoo import fields, models
 
 
 class SaleReport(models.Model):
-
     _inherit = "sale.report"
 
-    operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit')
+    operating_unit_id = fields.Many2one(
+        'operating.unit',
+        string='Operating Unit',
+        readonly=True
+    )
 
-    def _query(self, with_clause='', fields=False,  # noqa
-               groupby='', from_clause=''):
-        if not fields:
-            fields = {}
-        fields['operating_unit_id'] = \
-            ", s.operating_unit_id as operating_unit_id"
-        groupby += ', s.operating_unit_id'
-        return super(SaleReport, self)._query(with_clause, fields,
-                                              groupby, from_clause)
+    def _query(self, with_clause='', fields=None, groupby='', from_clause=''):
+        fields = dict(fields or {})
+        fields['operating_unit_id'] = ", s.operating_unit_id as operating_unit_id"
+        groupby = f"{groupby}, s.operating_unit_id"
+        return super()._query(with_clause, fields, groupby, from_clause)
