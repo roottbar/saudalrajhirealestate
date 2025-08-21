73f4251948acfabbe3ffc53ed25151d05b3b8454
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-21 15:36:52 +0300

    Update portal.py

--- renting/controllers/portal.py
+++ renting/controllers/portal.py
@@ -1,23 +1,19 @@
 import binascii
-
 from odoo import fields, http, SUPERUSER_ID, _
 from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
-# from odoo.addons.payment.controllers.portal import PaymentProcessing
-from odoo.addons.portal.controllers.mail import _message_post_helper
-from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
-from odoo.osv import expression
-from odoo import http
-from odoo.addons.sale.controllers.portal import CustomerPortal  # Import the class
+from odoo.addons.portal.controllers.portal import CustomerPortal
 
 
-class CustomCustomerPortal(CustomerPortal):  # Inherit in your custom class
+class CustomCustomerPortal(CustomerPortal):
 
     @http.route(['/my/orders/<int:order_id>/accept'], type='json', auth="public", website=True)
     def portal_quote_accept(self, order_id, access_token=None, name=None, signature=None):
-        # get from query string if not on json param
+        # استدعاء الدالة الأصلية من CustomerPortal
         res = super(CustomCustomerPortal, self).portal_quote_accept(order_id)
+
         access_token = access_token or request.httprequest.args.get('access_token')
+
         try:
             order_sudo = self._document_check_access('sale.order', order_id, access_token=access_token)
         except (AccessError, MissingError):
@@ -33,24 +29,29 @@ class CustomCustomerPortal(CustomerPortal):  # Inherit in your custom class
                 'signature': signature,
             })
             request.env.cr.commit()
-        except (TypeError, binascii.Error) as e:
+        except (TypeError, binascii.Error):
             return {'error': _('Invalid signature data.')}
 
+        # إذا لم يكن هناك دفعة مطلوبة
         if not order_sudo.has_to_be_paid():
             order_sudo.action_confirm()
             order_sudo._send_order_confirmation_mail()
 
-        pdf = request.env.ref('sale.action_report_saleorder').with_user(SUPERUSER_ID)._render_qweb_pdf([order_sudo.id])[
-            0]
+        # توليد ملف PDF للطلب
+        pdf = request.env.ref('sale.action_report_saleorder').with_user(SUPERUSER_ID)._render_qweb_pdf([order_sudo.id])[0]
 
-        _message_post_helper(
-            'sale.order', order_sudo.id, _('Order signed by %s') % (name,),
+        # إضافة رسالة على الطلب مع المرفق
+        order_sudo.message_post(
+            body=_('Order signed by %s' % name),
             attachments=[('%s.pdf' % order_sudo.name, pdf)],
-            **({'token': access_token} if access_token else {}))
+            **({'token': access_token} if access_token else {})
+        )
 
+        # إعادة توجيه للعميل مع رسالة النجاح
         query_string = '&message=sign_ok'
         if order_sudo.has_to_be_paid(True):
             query_string += '#allow_payment=yes'
+
         return {
             'force_refresh': True,
             'redirect_url': order_sudo.get_portal_url(query_string=query_string),
