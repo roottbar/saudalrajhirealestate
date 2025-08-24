19f8a3cab11fa4db197b61295213110ca187bccc
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-24 08:14:14 +0300

    Update rent_rental_pricing_inherit.py

--- renting/models/rent_rental_pricing_inherit.py
+++ renting/models/rent_rental_pricing_inherit.py
@@ -5,10 +5,12 @@ from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
-units = [('day', 'Days'),
-         ('week', 'Weeks'),
-         ('month', 'Months'),
-         ('year', 'Years')]
+units = [
+    ('day', 'Days'),
+    ('week', 'Weeks'),
+    ('month', 'Months'),
+    ('year', 'Years')
+]
 
 PERIOD_RATIO = {
     'hour': 1,
@@ -17,16 +19,20 @@ PERIOD_RATIO = {
 }
 
 
-class RentRentalPricing(models.Model):
-    _inherit = 'rental.pricing'
+class RentalPricing(models.Model):
+    _name = 'rental.pricing'
+    _description = 'Rental Pricing'
 
-    unit = fields.Selection(units, string='Unit', required='true')
+    name = fields.Char(string="Name", required=True)
+    duration = fields.Float(string="Duration", required=True, default=1.0)
+    unit = fields.Selection(units, string='Unit', required=True, default='day')
+    price = fields.Float(string="Price", required=True, default=0.0)
 
     def _compute_price(self, duration, unit):
         """Compute the price for a specified duration of the current pricing rule.
 
         :param float duration: duration in hours
-        :param str unit: duration unit (hour, day, week)
+        :param str unit: duration unit (hour, day, week, month, year)
         :return float: price
         """
         self.ensure_one()
@@ -35,22 +41,28 @@ class RentRentalPricing(models.Model):
         if unit != self.unit:
             if unit == 'month' or self.unit == 'month':
                 raise ValidationError(_("Conversion between Months and another duration unit are not supported!"))
-            converted_duration = math.ceil((duration * PERIOD_RATIO[unit]) / (self.duration * PERIOD_RATIO[self.unit]))
+            converted_duration = math.ceil(
+                (duration * PERIOD_RATIO[unit]) / (self.duration * PERIOD_RATIO[self.unit])
+            )
         else:
             converted_duration = math.ceil(duration / self.duration)
         return self.price * converted_duration
 
     def _compute_duration_vals(self, pickup_date, return_date):
+        """Compute duration values between two datetimes."""
         duration = return_date - pickup_date
         vals = dict(hour=(duration.days * 24 + duration.seconds / 3600))
         vals['day'] = math.ceil(vals['hour'] / 24)
         vals['week'] = math.ceil(vals['day'] / 7)
+
         duration_diff = relativedelta(return_date, pickup_date)
         months = 1 if duration_diff.days or duration_diff.hours or duration_diff.minutes else 0
         months += duration_diff.months
         months += duration_diff.years * 12
         vals['month'] = months
+
         years = 1 if duration_diff.months or duration_diff.days or duration_diff.hours or duration_diff.minutes else 0
         years += duration_diff.years
         vals['year'] = years
+
         return vals
