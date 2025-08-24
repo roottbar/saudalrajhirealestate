086119d548dc52a293fa743df578ec58f5464331
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-24 08:24:59 +0300

    Update rent_rental_wizard_inherit.py

--- renting/models/rent_rental_wizard_inherit.py
+++ renting/models/rent_rental_wizard_inherit.py
@@ -6,5 +6,15 @@ from odoo import models, fields
 class RentRentalWizardInherit(models.TransientModel):
     _inherit = 'rental.wizard'
 
-    duration_unit = fields.Selection([("hour", "Hours"), ("day", "Days"), ("week", "Weeks"), ("month", "Months"), ("year", "years")],
-                                     string="Unit", required=True, compute="_compute_duration")
+    duration_unit = fields.Selection(
+        [
+            ("hour", "Hours"),
+            ("day", "Days"),
+            ("week", "Weeks"),
+            ("month", "Months"),
+            ("year", "Years"),
+        ],
+        string="Unit",
+        required=True,
+        compute="_compute_duration"
+    )
