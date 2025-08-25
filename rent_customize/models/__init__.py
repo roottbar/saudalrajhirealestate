1b177d3bc158186f66c287465f9a7eb967899547
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-25 14:53:50 +0300

    Add files via upload

---
+++ rent_customize/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import rent_property_build
+from . import sales
+from . import product
+from . import sale_rental_schedule
+from . import rent_sale_state
+from . import move
+
