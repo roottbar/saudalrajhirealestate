bab90213390ab6d6190cc9bbc7f5f49d36429b33
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-08-28 13:12:07 +0300

    Update stock.py

--- multi_branches/models/stock.py
+++ multi_branches/models/stock.py
@@ -221,47 +221,9 @@ class StockQuant(models.Model):
     branch_id = fields.Many2one(related='location_id.branch_id', store=True, readonly=True, string='Branch')
 
 
-class Inventory(models.Model):
-    _inherit = "stock.inventory"
-
-    branch_id = fields.Many2one("res.branch", string='Branch', default=lambda self: self.env.user.branch_id)
-    location_ids = fields.Many2many(
-        'stock.location', string='Locations',
-        readonly=True, check_company=True,
-        states={'draft': [('readonly', False)]},
-        domain="[('company_id', '=', company_id), ('usage', 'in', ['internal', 'transit']), '|', ('branch_id', '=', branch_id), ('branch_id', '=', False)]")
-
-    @api.model
-    def default_get(self, fields):
-        res = super(Inventory, self).default_get(fields)
-        if res.get('location_id'):
-            location_branch = self.env['stock.location'].browse(res.get('location_id')).branch_id.id
-            if location_branch:
-                res['branch_id'] = location_branch
-        else:
-            res['branch_id'] = self.env.user.branch_id.id
-        return res
-
-    @api.onchange('company_id')
-    def _onchange_company_id(self):
-        if self.company_id:
-            branches = self.env.user.branch_ids.filtered(lambda m: m.company_id.id == self.company_id.id).ids
-            self.branch_id = False
-            if len(branches) > 0:
-                self.branch_id = branches[0]
-        else:
-            return {'domain': {'branch_id': []}}
-
-    def post_inventory(self):
-        # The inventory is posted as a single step which means quants cannot be moved from an internal location to another using an inventory
-        # as they will be moved to inventory loss, and other quants will be created to the encoded quant location. This is a normal behavior
-        # as quants cannot be reuse from inventory location (users can still manually move the products before/after the inventory if they want).
-        self.mapped('move_ids').filtered(lambda move: move.state != 'done')._action_done()
-        for move_id in self.move_ids:
-            account_move = self.env['account.move'].search([('stock_move_id', '=', move_id.id)])
-            account_move.write({'branch_id': self.branch_id.id})
-            for line in account_move.line_ids:
-                line.write({'branch_id': self.branch_id.id})
+# Note: stock.inventory model was removed in Odoo 16+
+# Inventory adjustments are now handled through stock.quant directly
+# This class has been removed to maintain Odoo 18 compatibility
 
 
 class StockRule(models.Model):
