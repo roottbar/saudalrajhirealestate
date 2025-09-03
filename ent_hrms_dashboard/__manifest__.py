7d39abd441f50c40dd3c9649cff6a989c511c6c6
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-09-03 10:18:41 +0300

    Update __manifest__.py

--- ent_hrms_dashboard/__manifest__.py
+++ ent_hrms_dashboard/__manifest__.py
@@ -21,7 +21,7 @@
 ########################################################################################
 {
     'name': "Enterprise Open HRMS - HR Dashboard",
-    'version': '15.0.1.0.0',
+    'version': '18.0.1.0.0',
     'summary': """Open HRMS - HR Dashboard""",
     'description': """Open HRMS - HR Dashboard""",
     'category': 'Generic Modules/Human Resources',
