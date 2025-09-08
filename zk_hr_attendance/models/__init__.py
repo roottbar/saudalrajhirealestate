5eb4cb5577a3616ce10cb81f5a976985181200a5
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-09-08 11:50:33 +0300

    Delete zk_hr_attendance directory

--- zk_hr_attendance/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-####################################
-# Author: Bashier Elbashier
-# Date: 27th February, 2021
-####################################
-
-from . import hr_employee
-from . import zk_hr_attendance_log
-from . import hr_attendance
