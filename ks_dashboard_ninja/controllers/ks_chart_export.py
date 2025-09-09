942555c26112c876b30071e59aff3e30ede7935b
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-09-10 00:30:27 +0300

    Update ks_chart_export.py

--- ks_dashboard_ninja/controllers/ks_chart_export.py
+++ ks_dashboard_ninja/controllers/ks_chart_export.py
@@ -5,7 +5,8 @@ import io
 import json
 import operator
 
-from odoo.addons.web.controllers.main import ExportFormat,serialize_exception, ExportXlsxWriter
+from odoo.addons.web.controllers.utils import serialize_exception
+from odoo.addons.web.controllers.export import ExportXlsxWriter
 from odoo.tools.translate import _
 from odoo import http
 from odoo.http import content_disposition, request
