91b415897377fa99507894c69b5b3e0efbd1d653
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-09-09 14:32:10 +0300

    Update __manifest__.py

--- user_log/__manifest__.py
+++ user_log/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'User Activity Log',
-    'version': '15.0.1.0.0',
+    'version': '18.0.1.0.0',
     'summary': 'Track and log user activities in the system',
     'description': """
         This module tracks and logs all user activities including create, write, unlink operations
