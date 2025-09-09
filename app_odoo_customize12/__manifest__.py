f457da42ea54c189bd68c7972417d7db35b9663f
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-09-09 09:37:59 +0300

    Update __manifest__.py

--- app_odoo_customize12/__manifest__.py
+++ app_odoo_customize12/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'odoo17 Tweak,Ai Employee,Boost,Customize All in One. Customize,UI,Boost,Security,Data',
-    'version': '15.0.0.0',
+    'version': '18.0.0.0',
     'author': 'odooai.cn',
     'category': 'Productivity',
     'website': 'https://www.odooai.cn',
