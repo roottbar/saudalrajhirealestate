147e2ccbe4aa85c83f95bccbd29fad82988562d9
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-09-14 15:01:30 +0300

    Update google_map_view.js

--- web_google_maps/static/src/js/view/google_map/google_map_view.js
+++ web_google_maps/static/src/js/view/google_map/google_map_view.js
@@ -1,74 +1,54 @@
-odoo.define('web_google_maps.GoogleMapView', function (require) {
-    'use strict';
+/** @odoo-module **/
 
-    const BasicView = require('web.BasicView');
-    const core = require('web.core');
-    const pyUtils = require('web.py_utils');
+import { registry } from "@web/core/registry";
+import { _t } from "@web/core/l10n/translation";
+import { evaluateExpr } from "@web/core/py_js/py";
 
-    const GoogleMapModel = require('web_google_maps.GoogleMapModel');
-    const GoogleMapRenderer = require('web_google_maps.GoogleMapRenderer').GoogleMapRenderer;
-    const GoogleMapController = require('web_google_maps.GoogleMapController');
-    const Utils = require('web_google_maps.Utils');
+import { GoogleMapModel } from "./google_map_model";
+import { GoogleMapRenderer } from "./google_map_renderer";
+import { GoogleMapController } from "./google_map_controller";
+import { parseMarkersColor } from "../utils";
 
-    const _lt = core._lt;
-
-    const GoogleMapView = BasicView.extend({
-        accesskey: 'm',
-        display_name: _lt('Google Map'),
-        icon: 'fa-map-o',
-        config: _.extend({}, BasicView.prototype.config, {
+export const googleMapView = {
+    type: "google_map",
+    display_name: _t("Google Map"),
+    icon: "fa fa-map-o",
+    multiRecord: true,
+    Controller: GoogleMapController,
+    Model: GoogleMapModel,
+    Renderer: GoogleMapRenderer,
+    searchMenuTypes: ["filter", "groupBy", "favorite"],
+    buttonTemplate: "web_google_maps.GoogleMapView.Buttons",
+    props(genericProps, view, config) {
+        const { arch, fields } = view;
+        const attrs = arch.attrs;
+        
+        const modes = ['geometry'];
+        const defaultMode = 'geometry';
+        const map_mode = attrs.mode && modes.includes(attrs.mode) ? attrs.mode : defaultMode;
+        
+        const colors = parseMarkersColor(attrs.colors);
+        
+        return {
+            ...genericProps,
             Model: GoogleMapModel,
             Renderer: GoogleMapRenderer,
             Controller: GoogleMapController,
-        }),
-        viewType: 'google_map',
-        mobile_friendly: true,
-        _map_mode: function () {
-            return ['geometry'];
-        },
-        init: function (viewInfo, params) {
-            this._super.apply(this, arguments);
+            arch,
+            fields,
+            mapMode: map_mode,
+            markerColor: attrs.color,
+            markerColors: colors,
+            fieldLat: attrs.lat,
+            fieldLng: attrs.lng,
+            gestureHandling: attrs.gesture_handling,
+            googleMapStyle: attrs.map_style || false,
+            disableClusterMarker: attrs.disable_cluster_marker ? evaluateExpr(attrs.disable_cluster_marker) : false,
+            sidebarTitle: attrs.sidebar_title || false,
+            sidebarSubtitle: attrs.sidebar_subtitle || false,
+            disableNavigation: attrs.disable_navigation ? evaluateExpr(attrs.disable_navigation) : false,
+        };
+    },
+};
 
-            const arch = this.arch;
-            const attrs = arch.attrs;
-            const activeActions = this.controllerParams.activeActions;
-
-            this.loadParams.limit = this.loadParams.limit || 80;
-            this.loadParams.type = 'list';
-
-            const modes = this._map_mode();
-            const defaultMode = 'geometry';
-            const map_mode = attrs.mode ? (modes.indexOf(attrs.mode) > -1 ? attrs.mode : defaultMode) : defaultMode;
-            this.rendererParams.arch = arch;
-            this.rendererParams.map_mode = map_mode;
-            this.rendererParams.record_options = {
-                editable: activeActions.edit,
-                deletable: activeActions.delete,
-                read_only_mode: params.readOnlyMode || true,
-            };
-            this.controllerParams.mode = arch.attrs.editable && !params.readonly ? 'edit' : 'readonly';
-            this.controllerParams.hasButtons = true;
-            if (attrs.options && !_.isObject(attrs.options)) {
-                attrs.options = attrs.options ? pyUtils.py_eval(attrs.options) : {};
-            }
-            const func_name = 'set_property_' + map_mode;
-            this[func_name].call(this, attrs);
-        },
-        set_property_geometry: function (attrs) {
-            const colors = Utils.parseMarkersColor(attrs.colors);
-            this.rendererParams.markerColor = attrs.color;
-            this.rendererParams.markerColors = colors;
-            this.rendererParams.fieldLat = attrs.lat;
-            this.rendererParams.fieldLng = attrs.lng;
-            this.rendererParams.gestureHandling = attrs.gesture_handling;
-            this.rendererParams.googleMapStyle = attrs.map_style || false;
-            this.rendererParams.disableClusterMarker =
-                attrs.disable_cluster_marker !== undefined ? !!pyUtils.py_eval(attrs.disable_cluster_marker) : false;
-            this.rendererParams.sidebarTitle = attrs.sidebar_title || false;
-            this.rendererParams.sidebarSubtitle = attrs.sidebar_subtitle || false;
-            this.rendererParams.disableNavigation = attrs.disable_navigation !== undefined ? !!pyUtils.py_eval(attrs.disable_navigation) : false;
-        },
-    });
-
-    return GoogleMapView;
-});
+registry.category("views").add("google_map", googleMapView);
