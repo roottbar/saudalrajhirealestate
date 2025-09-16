f77e00f4be02d4d45b460c7605ef9736b9589e90
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-09-16 15:18:37 +0300

    Delete hr_resume_ats directory

--- hr_resume_ats/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import hr_resume_analysis
-from . import hr_job_position
-from . import hr_resume_skill
-from . import hr_resume_education
-from . import hr_resume_experience
