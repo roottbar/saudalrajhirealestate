ed8733609ee5136233a66daaaa3a7fd28e612dfa
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-09-17 08:19:19 +0300

    Update __manifest__.py

--- hr_resume_ats/__manifest__.py
+++ hr_resume_ats/__manifest__.py
@@ -40,7 +40,7 @@
     'data': [
         'security/hr_resume_ats_security.xml',
         'security/ir.model.access.csv',
-        # 'data/ir_translation.xml',
+        'data/ir_translation.xml',
         'views/hr_resume_analysis_views.xml',
         'views/hr_job_position_views.xml',
         'views/res_config_settings_views.xml',
@@ -62,3 +62,4 @@
     'sequence': 95,
 
 }
+
