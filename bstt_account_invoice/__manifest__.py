942104845d9a9b706cded713f2e9cb10705f2c6f
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-09-18 15:57:30 +0300

    Update __manifest__.py

--- bstt_account_invoice/__manifest__.py
+++ bstt_account_invoice/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "BSTT",
     "license": "AGPL-3",
     "application": False,
-    "installable": True,
+    "installable": False,
     # "depends": ['base', 'web', 'l10n_gcc_invoice', 'l10n_sa', 'l10n_sa_invoice','hr'],
     "depends": ['base', 'web', 'l10n_gcc_invoice', 'l10n_sa', 'hr'],
     "data": [
