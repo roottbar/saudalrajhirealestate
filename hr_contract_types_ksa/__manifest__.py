cb1e8db87e81d3370cdfb14776eaaf787653f04f
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-09-29 10:21:31 +0300

    Add files via upload

---
+++ hr_contract_types_ksa/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Employee Contract Types (Saudi Arabia Version)',
+    'version': '15.0.1.0.0',
+    'category': 'Generic Modules/Human Resources',
+    'description': 'Employee Contracts Types,Saudi Arabia.',
+    'summary': 'Generate employee contract types and make it simple as is.',
+    'author': 'iUXING Technologies',
+    'company': 'iUXING',
+    'website': 'https://iuxing.com',
+    'maintainer': 'Rami A. Nasrallah',
+    'depends': ['hr', 'hr_contract'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/contract_view.xml',
+        'data/hr_contract_type_data.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'price': '0',
+    'currenct': 'EUR',
+    'support': 'info@iuxing.com',
+    'installable': True,
+    'application': False,
+    'license': 'AGPL-3',
+}
+
