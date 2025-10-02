62729202c6948dc4b4e69318a4ed0bf607668e03
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-10-02 09:02:05 +0300

    Update hr_employee.py

--- bstt_finger_print/models/hr_employee.py
+++ bstt_finger_print/models/hr_employee.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError, Warning
+from odoo.exceptions import UserError
 
 
 class Employee(models.Model):
