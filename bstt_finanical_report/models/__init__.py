bc270c99a941ddc4682fe2984cbe164cf782025b
roottbar <root@tbarholding.com>
2025-10-02 13:35:48 +0300

    Disable inheritance from removed model account.analytic.report in bstt_finanical_report

--- bstt_finanical_report/models/__init__.py
+++ bstt_finanical_report/models/__init__.py
@@ -1,3 +1,6 @@
 from . import account_move
 from . import account_asset
-from . import account_analytic_report
+# NOTE (Odoo 18): Base model 'account.analytic.report' no longer exists.
+# This customization inherits that model and breaks registry load.
+# Temporarily disabled to unblock deployment; rework needed against 'account.report'.
+# from . import account_analytic_report
