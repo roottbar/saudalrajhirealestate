76c31057fb1bdae94175842b6ee27082fe0d258c
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-10-02 09:10:28 +0300

    Update portal_access.py

--- dynamic_portal/models/portal_access.py
+++ dynamic_portal/models/portal_access.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError, Warning
+from odoo.exceptions import UserError
 from odoo.addons.base.models.ir_model import FIELD_TYPES
 
 
