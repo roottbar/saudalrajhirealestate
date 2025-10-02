eb0e71ca12a7494abaaedcb5b498416a7436e356
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-10-02 09:10:14 +0300

    Update portal_list_color.py

--- dynamic_portal/models/portal_list_color.py
+++ dynamic_portal/models/portal_list_color.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError, Warning
+from odoo.exceptions import UserError
 from odoo.addons.base.models.ir_model import FIELD_TYPES
 from odoo.exceptions import ValidationError
 from odoo.tools.safe_eval import safe_eval
