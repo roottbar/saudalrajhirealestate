424e38e4553d479982c90a467a736ab0be9395e9
roottbar <root@tbarholding.com>
2025-10-07 11:25:18 +0300

    fix(ks_dn_advance): remove merge markers; use isolated cursor for SQL queries

--- ks_dn_advance/models/ks_dashboard_advance_ninja_item.py
+++ ks_dn_advance/models/ks_dashboard_advance_ninja_item.py
@@ -209,11 +209,7 @@ class KsDashboardNinjaItemAdvance(models.Model):
                     try:
                         type_code = []
                         # conn = sql_db.db_connect(self.env.cr.dbname)
-<<<<<<< HEAD
                         new_env = self.pool.cursor()
-=======
-                        new_env = self.env.cr
->>>>>>> 18de2315 (Initial commit: Odoo 18 Update project with all modules and configurations)
                         if rec.ks_is_date_ranges:
                             start_date = rec.ks_query_start_date
                             end_date = rec.ks_query_end_date
@@ -294,11 +290,7 @@ class KsDashboardNinjaItemAdvance(models.Model):
             type_code = []
             # new_cr = self.pool.cursor()
             # conn = sql_db.db_connect(self.env.cr.dbname)
-<<<<<<< HEAD
             new_env = self.pool.cursor()
-=======
-            new_env = self.env.cr
->>>>>>> 18de2315 (Initial commit: Odoo 18 Update project with all modules and configurations)
             if self.ks_is_date_ranges:
                 ks_start_date = 'ks_start_date'
                 ks_end_date = 'ks_end_date'
@@ -519,7 +511,3 @@ class KsDashboardNinjaItemAdvance(models.Model):
         list_view_data = self.get_list_view_record(orderby, sort_order, ks_domain, ksoffset=initial_count - 1,
                                                    initial_count=initial_count)
         return list_view_data
-<<<<<<< HEAD
-=======
-
->>>>>>> 18de2315 (Initial commit: Odoo 18 Update project with all modules and configurations)
