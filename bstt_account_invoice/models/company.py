7b9f7bcf49a56337336d32b263a681a4e3b927e4
roottbar <root@tbarholding.com>
2025-10-09 14:01:06 +0300

    feat(qr): Support ZATCA Phase 2 TLV (19); ISO8601 timestamp; generate QR image

--- bstt_account_invoice/models/company.py
+++ bstt_account_invoice/models/company.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+﻿#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
@@ -6,6 +6,7 @@ from odoo.exceptions import UserError, Warning
 import base64
 import qrcode
 from io import BytesIO
+from datetime import datetime, time
 
 
 class Company(models.Model):
@@ -29,7 +30,7 @@ class AccountMove(models.Model):
 
     @api.depends('company_id.name', 'company_id.vat', 'invoice_date', 'amount_tax', 'amount_total')
     def _compute_l10n_sa_qr_code_str(self):
-        """حساب سلسلة QR وفق معيار ZATCA باستخدام TLV ثم Base64."""
+        """حساب سلسلة QR وفق معيار ZATCA باستخدام TLV ثم Base64. يدعم مرحلة 2 عند توفر الحقول (69)."""
         def _tlv(tag, value):
             value = value or ''
             vb = value.encode('utf-8')
@@ -39,11 +40,58 @@ class AccountMove(models.Model):
             if record.move_type in ('out_invoice', 'out_refund') and record.company_id:
                 seller = record.company_id.name or ''
                 vat = record.company_id.vat or ''
-                timestamp = str(fields.Datetime.now())
+                # وقت الإصدار بصيغة ISO8601 (UTC)
+                ts_value = record.invoice_date or record.create_date or fields.Datetime.now()
+                if isinstance(ts_value, datetime):
+                    ts_dt = ts_value
+                else:
+                    try:
+                        ts_dt = datetime.combine(ts_value, time())
+                    except Exception:
+                        ts_dt = fields.Datetime.now()
+                timestamp = ts_dt.strftime('%Y-%m-%dT%H:%M:%SZ')
                 total = '{:.2f}'.format(record.amount_total or 0.0)
                 vat_total = '{:.2f}'.format(record.amount_tax or 0.0)
 
-                payload = _tlv(1, seller) + _tlv(2, vat) + _tlv(3, timestamp) + _tlv(4, total) + _tlv(5, vat_total)
+                # حقول مرحلة 2 (TLV 6-9) إن وجدت في النموذج/الوحدات الأخرى
+                def _get_optional(field_names):
+                    for fname in field_names:
+                        if record._fields.get(fname):
+                            val = getattr(record, fname, False)
+                            if val:
+                                return str(val)
+                    return ''
+
+                invoice_hash = _get_optional([
+                    'invoice_hash', 'zatca_invoice_hash', 'qr_invoice_hash', 'crypto_invoice_hash'
+                ])
+                ecdsa_signature = _get_optional([
+                    'ecdsa_signature', 'zatca_signature', 'qr_signature', 'crypto_stamp_signature'
+                ])
+                public_key = _get_optional([
+                    'public_key', 'zatca_public_key', 'qr_public_key', 'crypto_stamp_public_key'
+                ])
+                signature_type = _get_optional([
+                    'signature_type', 'zatca_signature_type', 'qr_signature_type', 'crypto_stamp_signature_type'
+                ])
+
+                payload = (
+                    _tlv(1, seller)
+                    + _tlv(2, vat)
+                    + _tlv(3, timestamp)
+                    + _tlv(4, total)
+                    + _tlv(5, vat_total)
+                )
+
+                if invoice_hash:
+                    payload += _tlv(6, invoice_hash)
+                if ecdsa_signature:
+                    payload += _tlv(7, ecdsa_signature)
+                if public_key:
+                    payload += _tlv(8, public_key)
+                if signature_type:
+                    payload += _tlv(9, signature_type)
+
                 record.l10n_sa_qr_code_str = base64.b64encode(payload).decode('utf-8')
             else:
                 record.l10n_sa_qr_code_str = False
@@ -56,12 +104,12 @@ class AccountMove(models.Model):
                 qr = qrcode.QRCode(version=1, box_size=10, border=5)
                 qr.add_data(record.l10n_sa_qr_code_str)
                 qr.make(fit=True)
-                
+
                 img = qr.make_image(fill_color="black", back_color="white")
                 buffer = BytesIO()
                 img.save(buffer, format='PNG')
                 buffer.seek(0)
-                
+
                 record.qr_code = base64.b64encode(buffer.getvalue())
             else:
                 record.qr_code = False
