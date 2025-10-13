1c1ebca0ac47d33c0c7f1fd860371aa5098d8e55
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-13 15:35:28 +0300

    Add files via upload

---
+++ bstt_hr_attendance/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BSTT HR Attendance ",
+    'version': "18.0.1.0.0",
+    'summary': "Enhanced BSTT HR Attendance  module",
+    'description': "Enhanced BSTT HR Attendance  module for Odoo 18.0 by roottbar",
+    'category': "hr",
+    'author': "BSTT company",
+    'maintainer': "roottbar",
+    'depends': [
+        'base',
+        'hr_attendance',
+    ],
+    'data': [
+        'security/hr_attendance_security.xml',
+        'security/ir.model.access.csv',
+        'views/hr_attendance.xml',
+        'views/hr_attendance_batch.xml',
+    ],
+    'license': "LGPL-3",
+    'installable': False,  # Disabled for now - HR modules disabled
+    'auto_install': False,
+}
