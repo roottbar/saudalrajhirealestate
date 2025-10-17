4ffd97a7af4f9c5e5d121e92f8f2e2f2de9cdc4f
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 19:05:11 +0300

    Delete leave_contract_allocation directory

--- leave_contract_allocation/models/hr_leave_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, api, fields
-
-
-class LeaveType(models.Model):
-    _inherit = 'hr.leave.type'
-
-    allocation_per_contract = fields.Boolean(
-        help="If selected, the leave will be linked to the employee’s contract - when one year is completed, the employee’s balance will be zero.")
