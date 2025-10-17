eb4cdd1882015ce8d05f22b48b24360ec19e07b5
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 19:47:47 +0300

    Delete purchase_request directory

--- purchase_request/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import purchase_request
-from . import res_config_settings
-from . import purchase_order
-from . import product_category
-
--- purchase_request/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import purchase_request_quotation_wizard
-from . import purchase_request_reject_wizard
