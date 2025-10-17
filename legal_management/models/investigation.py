7fcc6557418bb927532bdde3653a920c7f4ad791
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 19:06:08 +0300

    Delete legal_management directory

--- legal_management/models/investigation.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-class Investigation(models.Model):
-    _name = "investigation"
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    _description = "Investigation"
-
-    state = fields.Selection([('new', 'New'), ('currently', 'Currently'), ('done', 'Done')], string="Status", default='new')
--- legal_management/models/type_court.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-class TypeCourt(models.Model):
-    _name = "type.court"
-    _description = "Type of Court"
-    _rec_name ="type_court"
-
-
-    type_court = fields.Char(string='Type of courts')
