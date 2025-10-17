7e8208f8817f6128cf0b130d2ba01af283962618
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 20:18:48 +0300

    Delete zk_hr_attendance directory

--- zk_hr_attendance/models/hr_attendance.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-
-class HRAttendance(models.Model):
-    _inherit = 'hr.attendance'
-
-    check_in_device = fields.Char(string='Check In Device')
-    check_out_device = fields.Char(string='Check Out Device')
