995743f80de8729cdf8658fb201e279eab4b403b
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 20:12:16 +0300

    Delete suprema_attendance directory

--- suprema_attendance/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import suprema_device
-from . import attendance_log
-from . import hr_employee
--- suprema_attendance/models/hr_employee.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class HrEmployeeInherit(models.Model):
-    _inherit = 'hr.employee'
-
-    biometric_id = fields.Char(string='Biometric ID')
