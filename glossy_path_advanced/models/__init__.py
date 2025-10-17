01ead7e8d56102ece70d31b914a1ca1ab1c2fd79
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 18:45:42 +0300

    Delete glossy_path_advanced directory

--- glossy_path_advanced/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import account_asset, employee, hr_payslip,res_religion, hr_job, ir_attachment
--- glossy_path_advanced/models/ir_attachment.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-
-class IrAttachment(models.Model):
-    _inherit = 'ir.attachment'
-
-    is_export_report = fields.Boolean(string="Export Report", copy=False, readonly=True)
