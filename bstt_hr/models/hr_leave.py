33dfa694b0b490fa00051f0186e85ff00aff8b78
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 13:49:00 +0300

    Delete bstt_hr directory

--- bstt_hr/models/hr_leave.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class HolidaysRequest(models.Model):
-    _inherit = "hr.leave"
-
-    is_travel_chk = fields.Boolean('يوجد مخصص سفر', default=False)
-    is_travel_done = fields.Boolean('تم صرف مخصص السفر', default=False)
