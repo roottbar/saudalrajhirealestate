bb2fb3653304002799753f4f809fabb24c5a1d15
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 18:25:13 +0300

    Delete disable_publisher_update directory

--- disable_publisher_update/models/update.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, models
-
-class PublisherWarrantyContract(models.AbstractModel):
-    _inherit = "publisher_warranty.contract"
-
-    def update_notification(self, cron_mode=True):
-        return True
