cdc2ec637e1c14412d1b2e24f1dc1a772cf6d5fc
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 19:52:19 +0300

    Delete rent_customize directory

--- rent_customize/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import rent_property_build
-from . import sales
-from . import product
-from . import sale_rental_schedule
-from . import rent_sale_state
-from . import move
-
