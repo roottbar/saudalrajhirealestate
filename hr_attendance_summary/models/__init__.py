4e5ab13bd7299560d05ba2262ed959f8e7489204
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 18:50:58 +0300

    Delete hr_attendance_summary directory

--- hr_attendance_summary/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import late_rule
-from . import hr_attendance_summary
-from . import hr_attendance
-from . import resource_calendar
-from . import hr_payslip
--- hr_attendance_summary/models/resource_calendar.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api, _
-
-
-class ResourceCalendar(models.Model):
-    _inherit = 'resource.calendar'
-
-    late_rule_ids = fields.Many2many('late.rule', string='Late Rules')
