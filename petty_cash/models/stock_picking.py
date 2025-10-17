2a7b6a9581c3e304f99c18f49a02ce91ed470042
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 19:34:01 +0300

    Delete petty_cash directory

--- petty_cash/models/stock_picking.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api
-
-
-class Picking(models.Model):
-    _inherit = "stock.picking"
-
-    petty_cash_id = fields.Many2one('petty.cash', string='Petty Cash')
--- petty_cash/report/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import petty_cash_lines_report
-from . import report_requests_feeding
