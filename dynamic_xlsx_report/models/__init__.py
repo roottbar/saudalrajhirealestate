12e3fbfd4e2b7ab47ab866d392a5f0352133c733
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-17 18:43:19 +0300

    Delete dynamic_xlsx_report directory

--- dynamic_xlsx_report/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import report_template
-from . import ir_actions_report
-from . import dynamic_xlsx_report
-from . import dynamic_pdf_report
-# from . import ir_ui_view
