85ef921f7e968e80ceb757c1b77d2318d1099c01
Alrabie-Bakhit <abakhit.c@tbarholding.com>
2025-10-18 18:22:37 +0300

    Delete branch directory

--- branch/models/stock_valuation_layer.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-
-class StockValuationLayer(models.Model):
-    _inherit = "stock.valuation.layer"
-
-    branch_id = fields.Many2one(related="stock_move_id.branch_id", string="Branch", readonly=True, store=True)
